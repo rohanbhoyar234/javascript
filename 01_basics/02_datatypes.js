fe8e4d9d02e1691a219ad5d100205fc63285fb53
rohanbhoyar234 <rohanbhoyar234@gmail.com>
2024-08-01 13:55:25 +0000

    javascript_datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict" // treat all JS code as newer version and remain same throught the file
+
+let name ="rohan"
+let age = 20
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => " "
+// boolean => true/false
+// null  // actually it is standalone value (also a datatype)
+// undefined 
+// symbol => for uniqueness
+
+
+// object
+
+// console.log(typeof "rohan");  // string
+// console.log(typeof (age));  // number
+// console.log(typeof null);   // object
+// console.log(typeof undefined);   // undefined
+
+
+
+
+
