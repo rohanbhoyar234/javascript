9c4cfbfefc522dddcf3de285877d6b9d753dec84
rohanbhoyar234 <rohanbhoyar234@gmail.com>
2024-08-01 14:52:07 +0000

    javascript_datatypeConversion_and_operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -32,10 +32,44 @@
 // // " " => false 
 // // "rohan" => true
 
-let someNumber = 13
-let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// let someNumber = 13
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+// ***************** operations ******************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(7+7)
+// console.log(7-7)
+// console.log(7*7)
+// console.log(33+7)
+// console.log(7**7)
+// console.log(9%7)
+
+// let str1 = " radhe"
+// let str2 = " radhe"
+// let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1"+2) // 12
+// console.log(1+"2") // 12
+// console.log("1"+2+3) // 123
+// console.log(2+3+"1")  // 51
+
+// console.log(true)  // output is true
+// console.log(+true)  // output is 1
+// console.log(true+) // gives error
+// console.log(+"") // output is 0
+
+
+
+
+
 
 
 
