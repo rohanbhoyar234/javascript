d8c5e14fb3311f5fc2985b655cf20fe443e8c58d
rohanbhoyar234 <rohanbhoyar234@gmail.com>
2024-08-01 14:24:33 +0000

    javascript_datatypeConversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,44 @@
+// let score = 16
+// console.log(typeof score);  // number
+
+// let score = "16"
+// console.log(typeof score); // string
+
+// let score ="21"
+// let score1 ="21abc"
+// let valueInNumber = Number(score1) // score1 get changed from string to number
+// console.log(typeof(valueInNumber))  
+// console.log(valueInNumber)  // output is NaN because it is mix of both number and string
+
+// let score2 = null
+// let valueInNumber2 = Number(score2)
+// console.log(valueInNumber2)  // output is 0
+
+// let score3 = undefined
+// let valueInNumber3 = Number(score3)
+// console.log(typeof valueInNumber3)  
+// console.log(valueInNumber3)  // output is NaN
+
+// let score4 = true
+// let valueInNumber4 = Number(score4)
+// console.log(typeof valueInNumber4)  
+// console.log(valueInNumber4)  // output is 1 and for false output is 0
+
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)   // o/p: true
+// // 1 => true ; 0 => false
+// // " " => false 
+// // "rohan" => true
+
+let someNumber = 13
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+
+
+
+
+
