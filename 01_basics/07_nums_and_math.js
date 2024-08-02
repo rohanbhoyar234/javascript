3c057242b7df1e39e797cd11ed2308d52431a8d4
rohanbhoyar234 <rohanbhoyar234@gmail.com>
2024-08-02 02:31:35 +0000

    javascript_nums_and_math

---
+++ 01_basics/07_nums_and_math.js
@@ -0,0 +1,40 @@
+const score =400
+// console.log(score)   // 400
+
+const balance = new Number(100)
+// console.log(balance)  // [Number: 100]
+
+// console.log(balance.toString().length)  // 3
+// console.log(balance.toFixed(2))  // 100.00
+
+const anotherNumber = 21.8978
+// console.log(anotherNumber.toPrecision(3))  // 21.9
+
+const otherNumber = 123.9897
+// console.log(otherNumber.toPrecision(3))  // 124
+
+const number = 1123.8966
+// console.log(number.toPrecision(3))  // 1.12e+3
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString())  // 1,000,000  (according to USA)
+// console.log(hundreds.toLocaleString('en-IN')) 
+
+// ************** Maths ****************
+
+// console.log(Math)
+// console.log(Math.abs(-4))  // 4
+// console.log(Math.round(4.3))  // 4
+// console.log(Math.round(4.6))  // 5
+// console.log(Math.ceil(4.2))  // 5
+// console.log(Math.floor(4.9))  // 4
+// console.log(Math.min(4,3,6,7,9))
+// console.log(Math.max(4,3,6,7,9))
+
+// console.log(Math.random())  // always give random value between 0-1
+
+const min = 10
+const max = 20
+
+// console.log(Math.floor(Math.random() * (max- min + 1))+min)
+
