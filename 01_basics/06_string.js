59207c7f710581dd468d07dc005e69b5a85857ab
rohanbhoyar234 <rohanbhoyar234@gmail.com>
2024-08-02 01:53:25 +0000

    javascript_strings

---
+++ 01_basics/06_string.js
@@ -0,0 +1,39 @@
+const name = "raghav"
+const repoCount = 16
+
+// console.log(name + repoCount + " value")     this type of method is outdated 
+// string interpolation
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+// another method for declaring string
+const gameName = new String('R16')
+// console.log(gameName[0])  // output is R
+// console.log(gameName.__proto__)
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(0))
+// console.log(gameName.indexOf('r'))   // output is -1 because there is no 'r' in string
+// console.log(gameName.indexOf('R'))   // 0
+
+
+const game = "rohan-hc"
+// const newString = game.substring(0,4)
+// console.log(newString)
+
+// const anotherName = game.slice(-7,4)
+// console.log(anotherName)
+
+// const newString1 = "    rohan    "
+// console.log(newString1)
+// console.log(newString1.trim())
+
+// if we gave space during url then browser automatically fill it with "%20" (url encoding)
+const url = "https://rohan.com/rohan%20bhoyar"
+// console.log(url.replace('%20','_'))
+// console.log(url.includes('rohan'))
+// console.log(url.includes('raghav'))
+
+// console.log(game.split(''))
+// console.log(game.split('-'))
+
+
