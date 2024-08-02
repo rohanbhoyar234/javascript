70672dd7079b83d23f7addf0d314a1450a8d7884
rohanbhoyar234 <rohanbhoyar234@gmail.com>
2024-08-02 01:16:51 +0000

    javascript_datatypeSummary

--- 01_basics/05_datatypeSummary.js
+++ 01_basics/05_datatypeSummary.js
@@ -39,6 +39,31 @@ const myFunction = function(){
     console.log("hello world");
 }
 
-console.log(typeof myFunction)  // function   also called as object function
+// console.log(typeof myFunction)  // function   also called as object function
 
 // all non primitive datatype has object return type 
+
+
+// *******************  memmory  ***********************
+// primitive => stack memory (we get a copy of a variable)
+// non-primitive => heap memory (we get a refrence)
+
+let rohan = "rohanname"
+let anotherName = rohan   // will get a copy of variable rohan
+// console.log(rohan)
+// console.log(anotherName)
+
+// anotherName ="rajat"
+// console.log(rohan)
+// console.log(anotherName)
+
+let userOne ={
+    email : "rohan.com",
+    upi : "user@16"
+}
+
+let userTwo = userOne // now userTwo will get the refrence of userOne
+userTwo.email = "raghav.com"     // email get changed in both 
+console.log(userOne.email)     // "raghav.com" 
+console.log(userTwo.email)      //  "raghav.com" 
+
