5922f165017c51f5876b9bc674404df95659135d
rohanbhoyar234 <rohanbhoyar234@gmail.com>
2024-08-06 12:39:33 +0000

    javascript_objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -24,5 +24,47 @@ const user = {
 const obj1 = {1: "a", 2: "b",}
 const obj2 = {3: "a", 4: "b"}
 
-const obj3 = {obj1,obj2}
-console.log(obj3)  // we are getting the output as object inside object like array inside array
+// const obj3 = {obj1,obj2}
+// console.log(obj3)  // we are getting the output as object inside object like array inside array
+
+// const obj3 = Object.assign({},obj1,obj2)
+// follow above syntax or we can also do it by spread the output will be same
+const obj3 = {...obj1,...obj2}
+// console.log(obj3)  // { '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
+
+
+// console.log(tinderUser)
+// console.log(Object.keys(tinderUser))   // [ 'id', 'name', 'isLoggedIn' ]   we get the array of keys
+// console.log(Object.values(tinderUser))   // [ '1216rb', 'samantha', true ]  we get the array of values
+// console.log(Object.entries(tinderUser))    //   [ [ 'id', '1216rb' ], [ 'name', 'samantha' ], [ 'isLoggedIn', true ] ]
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'))  // true
+
+
+// object destructuring  
+
+const course = {
+    courseName : " flute",
+    price: 1600,
+    courseInstructor : "krishna"
+}
+
+// course.courseInstructor
+const {courseInstructor:instructor,courseName:cname} = course
+// console.log(instructor)  // krishna
+// console.log(cname)   // flute
+
+// JSON API
+
+// {
+//     // this two courley braces is simplt known as JSON
+
+//     "name" ; "raghav",
+//     "course" ; " flute"
+// }
+
+[
+    // this is also an API
+    {},
+    {},
+    {}
+]
