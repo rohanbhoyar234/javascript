f21db6d85c33183dd61ce8a006f46b1eabd00b6d
rohanbhoyar234 <rohanbhoyar234@gmail.com>
2024-08-07 15:08:22 +0000

    javascript_for_loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,12 @@
+// for loop
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+// for(let i = 1; i<11;i++){
+//     console.log(`16 * ${i} = ${16*i}`)
+// }
+
+
