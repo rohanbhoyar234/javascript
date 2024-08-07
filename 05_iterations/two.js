07af7aa7f96c8dca2f030dd1d0a5fe782f96c115
rohanbhoyar234 <rohanbhoyar234@gmail.com>
2024-08-07 15:19:46 +0000

    javascript_while_and_do-while_loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,22 @@
+// while loop
+
+// while (condition) {
+    
+// }
+
+// let index = 0
+// while (index <= 10){
+//     console.log(`value of index is ${index}`)
+//     index = index+2
+// }
+
+// do-while loop 
+// do {
+    
+// } while (condition);
+
+// let score =1
+// do{
+//     console.log(`score is ${score}`)
+//     score++
+// }while(score<=10)
